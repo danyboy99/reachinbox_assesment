e0092b83aa6fb5cd30ff967466fdb6d1ac1fe032
Danilo99 <omotehinse99@gmail.com>
2024-06-28 11:22:52 +0100

    creating queue and workers with bullmq

--- server/outlookOauth.ts
+++
@@ -1,4 +0,0 @@
-import dotenv from "dotenv";
-import * as readline from "readline";
-import { Client } from "@microsoft/microsoft-graph-client";
-import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
